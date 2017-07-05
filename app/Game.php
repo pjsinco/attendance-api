079cb03da5fcaaef3b7a70d21671c4dca84c1307
pjsinco <psinco@gmail.com>
2017-07-05 15:08:40 -0500

    Housekeeping

--- app/Game.php
+++ app/Game.php
@@ -32,30 +32,4 @@ class Game extends Model
   {
     return $filters->apply($builder);
   }
-
-//  public function scopeTeam($query, $teamId)
-//  {
-//    return $query->where('home', $teamId);
-//  }
-//
-//  public function scopeYear($query, $year)
-//  {
-//    return $query->whereYear('date_time', '=', $year);
-//  }
-//
-//  public function scopeMonth($query, $month)
-//  {
-//    return $query->whereMonth('date_time', '=', $month);
-//  }
-//
-//  public function scopeDay($query, $day)
-//  {
-//    return $query->whereYear('date_time', $day);
-//  }
-//
-//  public function scopeVisiting($query, $teamId)
-//  {
-//    return $query->where('away', $teamId);
-//  }
-
 }
