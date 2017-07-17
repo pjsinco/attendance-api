f1814b155de100a21aeb9b723164f03948b82569
pjsinco <psinco@gmail.com>
2017-07-17 17:18:12 -0500

    Add some future routes and comment them out

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,26 @@ Route::group(['prefix' => '/v1'], function () {
 
     Route::get('/attendance', 'GamesController@index');
 
+    //Route::get('/venues/{abbrev?}', 'StadiumsController@index');
+    //Route::get('/venues/{id}', 'StadiumsController@show');
+
     Route::get('/teams/{abbrev?}', 'TeamsController@index');
 
   });
+
+  //Route::group(['prefix' => 'mls'], function() {
+    //Route::get('/venues', 'StadiumsController@index');
+  //});
+  
+  //Route::group(['prefix' => 'nfl'], function() {
+    //Route::get('/venues', 'StadiumsController@index');
+  //});
+
+  //Route::group(['prefix' => 'nba'], function() {
+    //Route::get('/venues', 'StadiumsController@index');
+  //});
+
+  //Route::group(['prefix' => 'nhl'], function() {
+    //Route::get('/venues', 'StadiumsController@index');
+  //});
 });
